6b1846f0a17704fcb7506a20ee1c5fa6144651fd
hyunseok <hyun104467@naver.com>
2023-08-26 01:35:21 +0900

    [chore]: 초기 세팅

---
+++ src/main/java/com/gachon/gothood/GotHoodApplication.java
@@ -0,0 +1,13 @@
+package com.gachon.gothood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotHoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotHoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gachon/gothood/GotHoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachon.gothood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotHoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
